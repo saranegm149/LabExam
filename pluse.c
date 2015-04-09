71a05a6b2e413bc4f4f8ca5835940430def9cf4e
saranegm149 <eng.saranegm149@gmail.com>
2015-04-09 13:15:27 +0200

    maintenance of old version

--- pluse.c
+++ pluse.c
@@ -2,13 +2,19 @@
  
 int main()
 {
-   int a, b, c;
+   int a, b, c ,v;
  
    printf("Enter two numbers to add\n");
-   scanf("%d%d",&a,&b);
- 
-   c = a + b;
- 
+   scanf("%d%d%v",&a,&b,&v);
+   a+=1;
+   b+=1;
+   c = a + b + v;
+   c-=3;
+   c++;
+   c+=a;
+   v-=2;
+   v+=3;
+   a--;
    printf("Sum of entered numbers = %d\n",c);
  
    return 0;
