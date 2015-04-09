608cc671519c918feebde013491ccf404684cf7c
saranegm149 <eng.saranegm149@gmail.com>
2015-04-09 13:24:22 +0200

    d6

--- pluse.c
+++ pluse.c
@@ -12,10 +12,14 @@ int main()
    c-=3;
    c++;
    c+=a;
+   v-=2;
+   v+=3;
+   a--;
+   b=a+b;
+   c=a+b+c;
    
-   
- 
    printf("Sum of entered numbers = %d\n",c);
+   printf("Sum of entered numbers = %d\n",a);
  
    return 0;
 }
